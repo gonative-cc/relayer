d5c4b4da3c1def3f2f7e32f5c8a660f26c6bbe46
sczembor <stanislaw.czembor@gmail.com>
2024-11-06 00:10:39 +0100

    remove log.fatal

--- cmd/native-btc/main.go
+++ cmd/native-btc/main.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/hex"
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/btcsuite/btcd/rpcclient"
@@ -22,17 +21,20 @@ const (
 func main() {
 	err := godotenv.Load("../../.env")
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		fmt.Println("Error loading .env file", err)
+		return
 	}
 
 	if len(os.Args) < 2 {
-		log.Fatal("Missing transaction file path")
+		fmt.Println("Missing transaction file path")
+		return
 	}
 	txFilePath := os.Args[1]
 
 	txHex, err := os.ReadFile(txFilePath)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Error loading transaction file", err)
+		return
 	}
 
 	connCfg := &rpcclient.ConnConfig{
@@ -45,26 +47,30 @@ func main() {
 
 	client, err := rpcclient.New(connCfg, nil)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Error creating rpc client", err)
+		return
 	}
 	defer client.Shutdown()
 
 	// Decode the transaction from hex
 	txBytes, err := hex.DecodeString(string(txHex))
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Error decoding transaction", err)
+		return
 	}
 
 	var msgTx wire.MsgTx
 	err = msgTx.Deserialize(bytes.NewReader(txBytes))
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Error deserializing transaction", err)
+		return
 	}
 
 	// Send the raw transaction
 	txHash, err := client.SendRawTransaction(&msgTx, false)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Error sending transaction", err)
+		return
 	}
 
 	fmt.Printf("Transaction broadcasted successfully. Transaction ID: %s\n", txHash.String())
