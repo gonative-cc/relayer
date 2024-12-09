c575c174c2e0f68534f4e78295c87f1d6d7be941
sczembor <stanislaw.czembor@gmail.com>
2024-12-09 14:03:06 +0100

    approveAndSign implementation + test
    Signed-off-by: sczembor <stanislaw.czembor@gmail.com>

--- ika/client.go
+++ ika/client.go
@@ -2,6 +2,7 @@ package ika
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/block-vision/sui-go-sdk/models"
 	"github.com/block-vision/sui-go-sdk/signer"
@@ -20,6 +21,12 @@ type Client struct {
 	Function  string
 	GasAddr   string
 	GasBudget string
+}
+
+// SignOutputEventData represents the structure of the parsed JSON data
+// in the SignOutputEvent.
+type SignOutputEventData struct {
+	Signatures [][]byte `json:"signatures"`
 }
 
 // NewClient creates a new Client instance
@@ -77,4 +84,97 @@ func (p *Client) UpdateLC(
 		},
 		RequestType: "WaitForLocalExecution",
 	})
+}
+
+// ApproveAndSign approves and signs a set of messages using the IKA network. Returns its signatures
+func (p *Client) ApproveAndSign(
+	ctx context.Context,
+	dwalletCapID string,
+	signMessagesID string,
+	messages [][]byte,
+	logger zerolog.Logger,
+) ([][]byte, error) {
+
+	//TODO: This function was only tested against dummy implementation of the dwallet module deployed locally.
+	//Once it is ready, test it again
+	req := models.MoveCallRequest{
+		Signer:          p.Signer.Address,
+		PackageObjectId: p.LcPackage,
+		Module:          p.Module,
+		Function:        "approve_messages",
+		TypeArguments:   []interface{}{},
+		Arguments: []interface{}{
+			dwalletCapID,
+			messages,
+		},
+		GasBudget: p.GasBudget,
+	}
+	messageApprovals, err := p.c.MoveCall(ctx, req)
+	if err != nil {
+		logger.Err(err).Msg("Error calling move function:")
+		return nil, err
+	}
+
+	req = models.MoveCallRequest{
+		Signer:          p.Signer.Address,
+		PackageObjectId: p.LcPackage,
+		Module:          p.Module,
+		Function:        "sign",
+		TypeArguments:   []interface{}{},
+		Arguments: []interface{}{
+			signMessagesID,
+			messageApprovals,
+		},
+		GasBudget: p.GasBudget,
+	}
+	resp, err := p.c.MoveCall(ctx, req)
+	if err != nil {
+		logger.Err(err).Msg("Error calling move function:")
+		return nil, err
+	}
+
+	response, err := p.c.SignAndExecuteTransactionBlock(ctx, models.SignAndExecuteTransactionBlockRequest{
+		TxnMetaData: resp,
+		PriKey:      p.Signer.PriKey,
+		Options: models.SuiTransactionBlockOptions{
+			ShowInput:    true,
+			ShowRawInput: true,
+			ShowEffects:  true,
+		},
+		RequestType: "WaitForLocalExecution",
+	})
+	if err != nil {
+		logger.Err(err).Msg("Error executing transaction block:")
+		return nil, err
+	}
+
+	events, err := p.c.SuiGetEvents(ctx, models.SuiGetEventsRequest{
+		Digest: response.Effects.TransactionDigest,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error getting events: %w", err)
+	}
+
+	return extractSignatures(events[0].ParsedJson["signatures"]), nil
+}
+
+// extractSignatures extracts bytes from the `ParsedJson` structure
+func extractSignatures(data interface{}) [][]byte {
+	var byteArrays [][]byte
+
+	if slice, ok := data.([]interface{}); ok {
+		for _, inner := range slice {
+			var byteArray []byte
+			if innerSlice, ok := inner.([]interface{}); ok {
+				for _, value := range innerSlice {
+					if num, ok := value.(float64); ok {
+						byteArray = append(byteArray, byte(int(num)))
+					}
+				}
+			}
+			byteArrays = append(byteArrays, byteArray)
+		}
+	}
+
+	return byteArrays
 }
