dbc89e7a07514d97785d2895265fc3ba485f7b06
udit-gulati <uditgulati6@gmail.com>
2024-12-16 15:14:55 +0530

    add function retry helpers
    Signed-off-by: udit-gulati <uditgulati6@gmail.com>

---
+++ reporter/retry.go
@@ -0,0 +1,96 @@
+package reporter
+
+import (
+	"crypto/rand"
+	"errors"
+	"math/big"
+	"os"
+	"time"
+
+	"cosmossdk.io/log"
+)
+
+// TODO add log formatters
+var logger = log.NewLogger(os.Stdout)
+
+// unrecoverableErrors is a list of errors which are unsafe and should not be retried.
+var unrecoverableErrors = []error{
+	// btclctypes.ErrHeaderParentDoesNotExist,
+	// btclctypes.ErrChainWithNotEnoughWork,
+	// btclctypes.ErrInvalidHeader,
+	// btcctypes.ErrProvidedHeaderDoesNotHaveAncestor,
+	// btcctypes.ErrInvalidHeader,
+	// btcctypes.ErrNoCheckpointsForPreviousEpoch,
+	// btcctypes.ErrInvalidCheckpointProof,
+	// checkpointingtypes.ErrBlsPrivKeyDoesNotExist,
+	// TODO Add more errors here
+}
+
+// expectedErrors is a list of errors which can safely be ignored and should not be retried.
+var expectedErrors = []error{
+	// btcctypes.ErrDuplicatedSubmission,
+	// btcctypes.ErrInvalidHeader,
+	// TODO Add more errors here
+}
+
+func isUnrecoverableErr(err error) bool {
+	for _, e := range unrecoverableErrors {
+		if errors.Is(err, e) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func isExpectedErr(err error) bool {
+	for _, e := range expectedErrors {
+		if errors.Is(err, e) {
+			return true
+		}
+	}
+
+	return false
+}
+
+// Do executes a func with retry
+// TODO: check if this is needed, because is not being used.
+func RetryDo(sleep time.Duration, maxSleepTime time.Duration, retryableFunc func() error) error {
+	if err := retryableFunc(); err != nil {
+		if isUnrecoverableErr(err) {
+			logger.Error("Skip retry, error unrecoverable", "err", err)
+			return err
+		}
+
+		if isExpectedErr(err) {
+			logger.Error("Skip retry, error expected", "err", err)
+			return nil
+		}
+
+		// Add some randomness to prevent thrashing
+		jitter, err := randDuration(int64(sleep))
+		if err != nil {
+			return err
+		}
+		sleep = sleep + jitter/2
+
+		if sleep > maxSleepTime {
+			logger.Info("retry timed out")
+			return err
+		}
+
+		logger.Info("starting exponential backoff", "sleep", sleep, "err", err)
+		time.Sleep(sleep)
+
+		return RetryDo(2*sleep, maxSleepTime, retryableFunc)
+	}
+	return nil
+}
+
+func randDuration(maxNumber int64) (dur time.Duration, err error) {
+	randNumber, err := rand.Int(rand.Reader, big.NewInt(maxNumber))
+	if err != nil {
+		return dur, err
+	}
+	return time.Duration(randNumber.Int64()), nil
+}
