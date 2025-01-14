0f917f0629507fb7f977f9d2a2346c96632ce65d
sczembor <stanislaw.czembor@gmail.com>
2025-01-14 16:12:57 +0100

    linter
    Signed-off-by: sczembor <stanislaw.czembor@gmail.com>

--- cmd/native-relayer/cli/utils.go
+++ cmd/native-relayer/cli/utils.go
@@ -17,9 +17,8 @@ func loadConfig(configFile string) (*Config, error) {
 	if err := viper.ReadInConfig(); err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
 			return nil, fmt.Errorf("config file not found: %w", err)
-		} else {
-			return nil, fmt.Errorf("error reading config file: %w", err)
 		}
+		return nil, fmt.Errorf("error reading config file: %w", err)
 	}
 	var config Config
 	if err := viper.Unmarshal(&config); err != nil {
