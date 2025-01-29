87f9160aa1944f59bf6fee972eb8c95cbd25bb14
sczembor <43810037+sczembor@users.noreply.github.com>
2025-01-29 18:49:34 +0100

    Update ika/errors.go
    Co-authored-by: Robert Zaremba <robert@zaremba.ch>
    Signed-off-by: sczembor <43810037+sczembor@users.noreply.github.com>

--- ika/errors.go
+++ ika/errors.go
@@ -3,4 +3,4 @@ package ika
 import "errors"
 
 // ErrEventParsing is returned when there is an error parsing events.
-var ErrEventParsing = errors.New("error parsing events")
+var ErrEventParsing = errors.New("can't parse Sui event")
