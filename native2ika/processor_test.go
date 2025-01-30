00a72ba6934e6f4528f7c83ee6a3d6e4ec38259b
sczembor <stanislaw.czembor@gmail.com>
2025-01-30 17:48:54 +0100

    table tests
    Signed-off-by: sczembor <stanislaw.czembor@gmail.com>

--- native2ika/processor_test.go
+++ native2ika/processor_test.go
@@ -43,59 +43,82 @@ func TestRun(t *testing.T) {
 	assert.NotNil(t, retrievedSignRequests[0].FinalSig)
 }
 
-func TestRun_NoPendingRequests(t *testing.T) {
-	mockIkaClient := new(ika.MockClient)
-	processor := newIkaProcessor(t, mockIkaClient)
-
-	err := processor.Run(context.Background())
-	assert.Nil(t, err)
-	assert.NoError(t, err)
+type testRunCase struct {
+	name          string
+	ikaClient     ika.Client
+	setupDB       func(t *testing.T, db dal.DB)
+	expectedError string
+	assertions    func(t *testing.T, processor *Processor)
 }
 
-func TestRun_IKAClientError(t *testing.T) {
-	mockIkaClient := new(ika.MockClient)
-	processor := newIkaProcessor(t, mockIkaClient)
-	daltest.PopulateSignRequests(t, processor.db)
+func TestRun_EdgeCases(t *testing.T) {
+	testCases := []testRunCase{
+		{
+			name:      "NoPendingRequests",
+			ikaClient: new(ika.MockClient),
+			setupDB:   func(t *testing.T, db dal.DB) {}, // No need to populate the database
+		},
+		{
+			name: "IKAClientError",
+			ikaClient: func() ika.Client {
+				mockIkaClient := new(ika.MockClient)
+				mockIkaClient.On("ApproveAndSign", mock.Anything, mock.Anything, mock.Anything, mock.Anything).
+					Return(nil, "", errors.New("ika client error"))
+				return mockIkaClient
+			}(),
+			setupDB: func(t *testing.T, db dal.DB) {
+				daltest.PopulateSignRequests(t, db)
+			},
+			expectedError: "failed calling ApproveAndSign",
+		},
+		{
+			name: "Success",
+			ikaClient: func() ika.Client {
+				mockIkaClient := new(ika.MockClient)
+				mockIkaClient.On("ApproveAndSign", mock.Anything, mock.Anything, mock.Anything, mock.Anything).
+					Return([][]byte{[]byte("signature")}, "txDigest", nil)
+				return mockIkaClient
+			}(),
+			setupDB: func(t *testing.T, db dal.DB) {
+				daltest.PopulateSignRequests(t, db)
+			},
+			assertions: func(t *testing.T, processor *Processor) {
+				signRequests, err := processor.db.GetPendingIkaSignRequests()
+				assert.NoError(t, err)
+				assert.Equal(t, 0, len(signRequests)) // All requests should be processed
+			},
+		},
+		{
+			name: "EmptyPayload",
+			ikaClient: func() ika.Client {
+				mockIkaClient := new(ika.MockClient)
+				mockIkaClient.On("ApproveAndSign", mock.Anything, mock.Anything, mock.Anything, mock.Anything).
+					Return([][]byte{[]byte("signature")}, "txDigest", nil)
+				return mockIkaClient
+			}(),
+			setupDB: func(t *testing.T, db dal.DB) {
+				request := dal.IkaSignRequest{ID: 1, Payload: make([]byte, 0), DWalletID: "dwallet1",
+					UserSig: "user_sig1", FinalSig: nil, Timestamp: time.Now().Unix()}
+				err := db.InsertIkaSignRequest(request)
+				assert.NoError(t, err)
+			},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			processor := newIkaProcessor(t, tc.ikaClient)
+			tc.setupDB(t, processor.db)
 
-	mockIkaClient.On("ApproveAndSign", mock.Anything, mock.Anything, mock.Anything, mock.Anything).
-		Return(nil, "", errors.New("ika client error"))
+			err := processor.Run(context.Background())
+			if tc.expectedError != "" {
+				assert.ErrorContains(t, err, tc.expectedError)
+			} else {
+				assert.NoError(t, err)
+			}
 
-	err := processor.Run(context.Background())
-	assert.ErrorContains(t, err, "failed calling ApproveAndSign")
-}
-
-func TestRun_Success(t *testing.T) {
-	mockIkaClient := new(ika.MockClient)
-	processor := newIkaProcessor(t, mockIkaClient)
-	daltest.PopulateSignRequests(t, processor.db)
-
-	signRequests, err := processor.db.GetPendingIkaSignRequests()
-	assert.NoError(t, err)
-	assert.Equal(t, 2, len(signRequests))
-
-	mockIkaClient.On("ApproveAndSign", mock.Anything, mock.Anything, mock.Anything, mock.Anything).
-		Return([][]byte{[]byte("signature")}, "txDigest", nil)
-
-	err = processor.Run(context.Background())
-	assert.NoError(t, err)
-
-	signRequests, err = processor.db.GetPendingIkaSignRequests()
-	assert.NoError(t, err)
-	assert.Equal(t, 0, len(signRequests)) // All requests should be processed
-}
-
-func TestRun_EmptyPayload(t *testing.T) {
-	mockIkaClient := new(ika.MockClient)
-	processor := newIkaProcessor(t, mockIkaClient)
-	request := dal.IkaSignRequest{ID: 1, Payload: make([]byte, 0), DWalletID: "dwallet1",
-		UserSig: "user_sig1", FinalSig: nil, Timestamp: time.Now().Unix()}
-
-	err := processor.db.InsertIkaSignRequest(request)
-	assert.NoError(t, err)
-
-	mockIkaClient.On("ApproveAndSign", mock.Anything, mock.Anything, mock.Anything, mock.Anything).
-		Return([][]byte{[]byte("signature")}, "txDigest", nil)
-
-	err = processor.Run(context.Background())
-	assert.NoError(t, err)
+			if tc.assertions != nil {
+				tc.assertions(t, processor)
+			}
+		})
+	}
 }
