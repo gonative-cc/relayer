7f6609d40a10cb39c4ddb857cd6bf5a0c80fd615
udit-gulati <uditgulati6@gmail.com>
2025-02-12 17:10:20 +0530

    refactor cli
    Signed-off-by: udit-gulati <uditgulati6@gmail.com>

--- cmd/bitcoin-spv/cli.go
+++ cmd/bitcoin-spv/cli.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 
-	"github.com/filecoin-project/go-jsonrpc"
 	"github.com/gonative-cc/relayer/bitcoinspv"
 	"github.com/gonative-cc/relayer/bitcoinspv/config"
 	"github.com/gonative-cc/relayer/btcwrapper"
@@ -36,83 +35,16 @@ func CmdStart() *cobra.Command {
 		Use:   "start",
 		Short: "Runs the bitcoin-spv relayer",
 		Run: func(_ *cobra.Command, _ []string) {
-			var (
-				err          error
-				cfg          config.Config
-				btcClient    *btcwrapper.Client
-				nativeClient *lcclient.Client
-				spvRelayer   *bitcoinspv.Relayer
-				nativeCloser jsonrpc.ClientCloser
-			)
+			cfg, rootLogger := initConfig(cfgFile)
+			btcClient := initBTCClient(cfg, rootLogger)
+			nativeClient, nativeCloser := initNativeClient(cfg)
 
-			cfg, err = config.New(cfgFile)
-			if err != nil {
-				panic(fmt.Errorf("failed to load config: %w", err))
-			}
-			rootLogger, err := cfg.CreateLogger()
-			if err != nil {
-				panic(fmt.Errorf("failed to create logger: %w", err))
-			}
+			logTipBlock(btcClient, rootLogger)
 
-			// create BTC client and connect to BTC server
-			// Note that bitcoin-spv relayer needs to subscribe to new BTC blocks
-			btcClient, err = btcwrapper.NewClientWithBlockSubscriber(
-				&cfg.BTC,
-				cfg.Relayer.SleepDuration,
-				cfg.Relayer.MaxSleepDuration,
-				rootLogger,
-			)
-			if err != nil {
-				panic(fmt.Errorf("failed to open BTC client: %w", err))
-			}
-
-			tipBlock, err := btcClient.GetTipBlock()
-			if err != nil {
-				panic(fmt.Errorf("failed to get chain tip block: %w", err))
-			}
-
-			rootLogger.Info("got tip block",
-				zap.String("hash", tipBlock.Hash),
-				zap.Int64("height", tipBlock.Height),
-				zap.Int64("time", tipBlock.Time))
-
-			nativeClient, nativeCloser, err = lcclient.New(cfg.Native.RPCEndpoint)
-			if err != nil {
-				panic(fmt.Errorf("failed to open Native client: %w", err))
-			}
-
-			spvRelayer, err = bitcoinspv.New(
-				&cfg.Relayer,
-				rootLogger,
-				btcClient,
-				nativeClient,
-				cfg.Relayer.SleepDuration,
-				cfg.Relayer.MaxSleepDuration,
-			)
-			if err != nil {
-				panic(fmt.Errorf("failed to create bitcoin-spv relayer: %w", err))
-			}
-
-			// start normal-case execution
+			spvRelayer := initSPVRelayer(cfg, rootLogger, btcClient, nativeClient)
 			spvRelayer.Start()
 
-			// SIGINT handling stuff
-			addInterruptHandler(func() {
-				rootLogger.Info("Stopping relayer...")
-				spvRelayer.Stop()
-				rootLogger.Info("Relayer shutdown")
-			})
-			addInterruptHandler(func() {
-				rootLogger.Info("Stopping BTC client...")
-				btcClient.Stop()
-				btcClient.WaitForShutdown()
-				rootLogger.Info("BTC client shutdown")
-			})
-			addInterruptHandler(func() {
-				rootLogger.Info("Stopping Native client...")
-				nativeCloser()
-				rootLogger.Info("Native client shutdown")
-			})
+			setupShutdown(rootLogger, spvRelayer, btcClient, nativeCloser)
 
 			<-interruptHandlersDone
 			rootLogger.Info("Shutdown complete")
@@ -120,4 +52,93 @@ func CmdStart() *cobra.Command {
 	}
 	cmd.Flags().StringVar(&cfgFile, "config", config.DefaultConfigFile(), "config file")
 	return cmd
+}
+
+func initConfig(cfgFile string) (*config.Config, *zap.Logger) {
+	cfg, err := config.New(cfgFile)
+	if err != nil {
+		panic(fmt.Errorf("failed to load config: %w", err))
+	}
+	rootLogger, err := cfg.CreateLogger()
+	if err != nil {
+		panic(fmt.Errorf("failed to create logger: %w", err))
+	}
+	return &cfg, rootLogger
+}
+
+func initBTCClient(cfg *config.Config, rootLogger *zap.Logger) *btcwrapper.Client {
+	btcClient, err := btcwrapper.NewClientWithBlockSubscriber(
+		&cfg.BTC,
+		cfg.Relayer.SleepDuration,
+		cfg.Relayer.MaxSleepDuration,
+		rootLogger,
+	)
+	if err != nil {
+		panic(fmt.Errorf("failed to open BTC client: %w", err))
+	}
+	return btcClient
+}
+
+func logTipBlock(btcClient *btcwrapper.Client, rootLogger *zap.Logger) {
+	latestBTCBlock, err := btcClient.GetTipBlock()
+	if err != nil {
+		panic(fmt.Errorf("failed to get chain tip block: %w", err))
+	}
+
+	rootLogger.Info("Got tip block",
+		zap.String("hash", latestBTCBlock.Hash),
+		zap.Int64("height", latestBTCBlock.Height),
+		zap.Int64("time", latestBTCBlock.Time))
+}
+
+func initNativeClient(cfg *config.Config) (*lcclient.Client, func()) {
+	nativeClient, nativeCloser, err := lcclient.New(cfg.Native.RPCEndpoint)
+	if err != nil {
+		panic(fmt.Errorf("failed to open Native client: %w", err))
+	}
+	return nativeClient, nativeCloser
+}
+
+func initSPVRelayer(
+	cfg *config.Config,
+	rootLogger *zap.Logger,
+	btcClient *btcwrapper.Client,
+	nativeClient *lcclient.Client,
+) *bitcoinspv.Relayer {
+	spvRelayer, err := bitcoinspv.New(
+		&cfg.Relayer,
+		rootLogger,
+		btcClient,
+		nativeClient,
+		cfg.Relayer.SleepDuration,
+		cfg.Relayer.MaxSleepDuration,
+	)
+	if err != nil {
+		panic(fmt.Errorf("failed to create bitcoin-spv relayer: %w", err))
+	}
+	return spvRelayer
+}
+
+func setupShutdown(
+	rootLogger *zap.Logger,
+	spvRelayer *bitcoinspv.Relayer,
+	btcClient *btcwrapper.Client,
+	nativeCloser func(),
+) {
+	addInterruptHandler(func() {
+		rootLogger.Info("Stopping relayer...")
+		spvRelayer.Stop()
+		rootLogger.Info("Relayer shutdown")
+	})
+	addInterruptHandler(func() {
+		rootLogger.Info("Stopping BTC client...")
+		btcClient.Stop()
+		btcClient.WaitForShutdown()
+		rootLogger.Info("BTC client shutdown")
+	})
+	addInterruptHandler(func() {
+		rootLogger.Info("Stopping Native client...")
+		nativeCloser()
+		rootLogger.Info("Native client shutdown")
+	})
 }
