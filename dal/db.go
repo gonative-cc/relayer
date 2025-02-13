b232b4141f8d232a39f6347a218d819a2780d72f
sczembor <stanislaw.czembor@gmail.com>
2025-02-13 14:15:44 +0100

    draft of sqlc
    Signed-off-by: sczembor <stanislaw.czembor@gmail.com>

--- dal/db.go
+++ dal/db.go
@@ -1,52 +1,17 @@
 package dal
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
+	"os"
 	"sync"
 	"time"
 
+	"github.com/gonative-cc/relayer/dal/internal"
+
 	_ "github.com/mattn/go-sqlite3" // Import the SQLite driver
 )
-
-// IkaSignRequest represents a row in the `ika_sign_requests` table.
-type IkaSignRequest struct {
-	// ID is a sign request reported and managed by Native
-	ID        uint64    `json:"id"`
-	Payload   Payload   `json:"payload"`
-	DWalletID string    `json:"dwallet_id"`
-	UserSig   string    `json:"user_sig"`
-	FinalSig  Signature `json:"final_sig"`
-	Timestamp int64     `json:"time"`
-}
-
-// IkaTx represents a row in the `ika_txs` table.
-type IkaTx struct {
-	// SrID is the IkaSignRequest.ID (from Native)
-	SrID      uint64      `json:"sr_id"`
-	Status    IkaTxStatus `json:"status"`
-	IkaTxID   string      `json:"ika_tx_id"`
-	Timestamp int64       `json:"timestamp"`
-	Note      string      `json:"note"`
-}
-
-// BitcoinTx represents a row in the `bitcoin_txs` table.
-type BitcoinTx struct {
-	// SrID is the IkaSignRequest.ID (from Native)
-	SrID      uint64          `json:"sr_id"`
-	Status    BitcoinTxStatus `json:"status"`
-	BtcTxID   []byte          `json:"btc_tx_id"`
-	Timestamp int64           `json:"timestamp"`
-	Note      string          `json:"note"`
-}
-
-// BitcoinTxInfo holds the relevant information for a Bitcoin transaction.
-type BitcoinTxInfo struct {
-	// TxID reported by Bitcoin
-	TxID    uint64          `json:"tx_id"`
-	BtcTxID []byte          `json:"btc_tx_id"`
-	Status  BitcoinTxStatus `json:"status"`
-}
 
 // IkaTxStatus represents the different states of a native transaction.
 type IkaTxStatus byte
@@ -77,6 +42,7 @@ type Signature = []byte
 type DB struct {
 	conn  *sql.DB
 	mutex *sync.RWMutex
+	internal.Querier
 }
 
 // NewDB creates a new DB instance
@@ -85,7 +51,8 @@ func NewDB(dbPath string) (DB, error) {
 	if err != nil {
 		return DB{}, fmt.Errorf("dal: can't open sqlite3: %w", err)
 	}
-	return DB{conn: conn, mutex: &sync.RWMutex{}}, err
+	queries := internal.New(conn)
+	return DB{conn: conn, mutex: &sync.RWMutex{}, Querier: queries}, err
 }
 
 // InitDB initializes the database and creates the tables.
@@ -93,68 +60,25 @@ func (db DB) InitDB() error {
 	db.mutex.Lock()
 	defer db.mutex.Unlock()
 
-	const createIkaSignRequestsTableSQL = `
-		CREATE TABLE IF NOT EXISTS ika_sign_requests (
-			id INTEGER PRIMARY KEY,
-			payload BLOB NOT NULL,
-			dwallet_id TEXT NOT NULL,
-			user_sig TEXT NOT NULL,
-			final_sig BLOB,
-			timestamp INTEGER NOT NULL
-		)`
-	const createIkaTxsTableSQL = `
-		CREATE TABLE IF NOT EXISTS ika_txs (
-			sr_id INTEGER NOT NULL,  -- sign request
-			status INTEGER NOT NULL,
-			ika_tx_id TEXT NOT NULL,
-			timestamp INTEGER NOT NULL,
-			note TEXT,
-			PRIMARY KEY (sr_id, ika_tx_id),
-			FOREIGN KEY (sr_id) REFERENCES ika_sign_requests (id)
-		)`
-
-	const createBitcoinTxsTableSQL = `
-		CREATE TABLE IF NOT EXISTS bitcoin_txs (
-			sr_id INTEGER NOT NULL,
-			status INTEGER NOT NULL,
-			btc_tx_id BlOB NOT NULL,
-			timestamp INTEGER NOT NULL,
-			note TEXT,
-			PRIMARY KEY (sr_id, btc_tx_id),
-			FOREIGN KEY (sr_id) REFERENCES ika_sign_requests (id)
-		)`
-	_, err := db.conn.Exec(createIkaSignRequestsTableSQL)
+	ctx := context.Background()
+	schema, err := os.ReadFile("schema.sql")
 	if err != nil {
-		return fmt.Errorf("dal: creating ika_sign_requests table: %w", err)
+		return fmt.Errorf("dal: reading schema file: %w", err)
 	}
-	_, err = db.conn.Exec(createIkaTxsTableSQL)
+	_, err = db.conn.ExecContext(ctx, string(schema))
 	if err != nil {
-		return fmt.Errorf("dal: creating ika_txs table: %w", err)
-	}
-	_, err = db.conn.Exec(createBitcoinTxsTableSQL)
-	if err != nil {
-		return fmt.Errorf("dal: creating bitcoin_txs table: %w", err)
+		return fmt.Errorf("dal: creating tables: %w", err)
 	}
 	return nil
 }
 
 // InsertIkaSignRequest inserts a new transaction into the database
-func (db DB) InsertIkaSignRequest(signReq IkaSignRequest) error {
+func (db DB) InsertIkaSignRequest(ctx context.Context, signReq internal.IkaSignRequest) error {
 	db.mutex.Lock()
 	defer db.mutex.Unlock()
 
-	const insertIkaSignRequestSQL = `
-		INSERT INTO ika_sign_requests (id, payload, dwallet_id, user_sig, final_sig, timestamp) 
-		VALUES (?, ?, ?, ?, ?, ?)`
-	_, err := db.conn.Exec(
-		insertIkaSignRequestSQL,
-		signReq.ID,
-		signReq.Payload,
-		signReq.DWalletID,
-		signReq.UserSig,
-		signReq.FinalSig,
-		signReq.Timestamp,
-	)
+	params := internal.InsertIkaSignRequestParams(signReq)
+	err := db.Querier.InsertIkaSignRequest(ctx, &params)
 	if err != nil {
 		return fmt.Errorf("dal: inserting ika_sign_request: %w", err)
 	}
@@ -162,14 +86,12 @@ func (db DB) InsertIkaSignRequest(signReq IkaSignRequest) error {
 }
 
 // InsertIkaTx inserts a new Ika transaction into the database.
-func (db DB) InsertIkaTx(tx IkaTx) error {
+func (db DB) InsertIkaTx(ctx context.Context, tx internal.IkaTx) error {
 	db.mutex.Lock()
 	defer db.mutex.Unlock()
 
-	const insertIkaTxSQL = `
-		INSERT INTO ika_txs (sr_id, status, ika_tx_id, timestamp, note)
-		VALUES (?, ?, ?, ?, ?)`
-	_, err := db.conn.Exec(insertIkaTxSQL, tx.SrID, tx.Status, tx.IkaTxID, tx.Timestamp, tx.Note)
+	params := internal.InsertIkaTxParams(tx)
+	err := db.Querier.InsertIkaTx(ctx, &params)
 	if err != nil {
 		return fmt.Errorf("dal: inserting ika_tx: %w", err)
 	}
@@ -177,14 +99,12 @@ func (db DB) InsertIkaTx(tx IkaTx) error {
 }
 
 // InsertBtcTx inserts a new Bitcoin transaction into the database.
-func (db DB) InsertBtcTx(tx BitcoinTx) error {
+func (db DB) InsertBtcTx(ctx context.Context, tx internal.BitcoinTx) error {
 	db.mutex.Lock()
 	defer db.mutex.Unlock()
 
-	const insertBtcTxSQL = `
-		INSERT INTO bitcoin_txs (sr_id, status, btc_tx_id, timestamp, note)
-		VALUES (?, ?, ?, ?, ?)`
-	_, err := db.conn.Exec(insertBtcTxSQL, tx.SrID, tx.Status, tx.BtcTxID, tx.Timestamp, tx.Note)
+	params := internal.InsertBtcTxParams(tx)
+	err := db.Querier.InsertBtcTx(ctx, &params)
 	if err != nil {
 		return fmt.Errorf("dal: inserting bitcoin_tx: %w", err)
 	}
@@ -192,106 +112,63 @@ func (db DB) InsertBtcTx(tx BitcoinTx) error {
 }
 
 // GetIkaSignRequestByID retrives a signature request by its id
-func (db DB) GetIkaSignRequestByID(id uint64) (*IkaSignRequest, error) {
+func (db DB) GetIkaSignRequestByID(ctx context.Context, id uint64) (*internal.IkaSignRequest, error) {
 	db.mutex.RLock()
 	defer db.mutex.RUnlock()
 
-	const getIkaSignRequestByIDSQL = `
-		SELECT id, payload, dwallet_id, user_sig, final_sig, timestamp
-		FROM ika_sign_requests
-		WHERE id = ?`
-	row := db.conn.QueryRow(getIkaSignRequestByIDSQL, id)
-	var signReq IkaSignRequest
-	err := row.Scan(
-		&signReq.ID,
-		&signReq.Payload,
-		&signReq.DWalletID,
-		&signReq.UserSig,
-		&signReq.FinalSig,
-		&signReq.Timestamp,
-	)
+	signReq, err := db.Querier.GetIkaSignRequestByID(ctx, int64(id))
 	if err != nil {
-		if err == sql.ErrNoRows {
-			return nil, nil
+		if err == sql.ErrNoRows { // TODO test if we can just return it since we are using `emit_empty_slices: true` flag
+			return nil, nil // Return nil, nil for not found
 		}
 		return nil, fmt.Errorf("dal: getting ika_sign_request by id: %w", err)
 	}
-	return &signReq, nil
+	return signReq, nil
 }
 
 // GetIkaTx retrieves an Ika transaction by its primary key (sr_id and ika_tx_id).
-func (db DB) GetIkaTx(signRequestID uint64, ikaTxID string) (*IkaTx, error) {
+func (db DB) GetIkaTx(ctx context.Context, signRequestID uint64, ikaTxID string) (*internal.IkaTx, error) {
 	db.mutex.RLock()
 	defer db.mutex.RUnlock()
 
-	const getIkaTxByTxIDAndIkaTxIDSQL = `
-        SELECT sr_id, status, ika_tx_id, timestamp, note
-        FROM ika_txs
-        WHERE sr_id = ? AND ika_tx_id = ?`
-	row := db.conn.QueryRow(getIkaTxByTxIDAndIkaTxIDSQL, signRequestID, ikaTxID)
-	var ikaTx IkaTx
-	err := row.Scan(&ikaTx.SrID, &ikaTx.Status, &ikaTx.IkaTxID, &ikaTx.Timestamp, &ikaTx.Note)
+	ikaTx, err := db.Querier.GetIkaTx(ctx, &internal.GetIkaTxParams{SrID: int64(signRequestID), IkaTxID: ikaTxID})
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return nil, nil
 		}
 		return nil, fmt.Errorf("dal: getting ika_tx: %w", err)
 	}
-	return &ikaTx, nil
+	return ikaTx, nil
 }
 
 // GetBitcoinTx retrieves a Bitcoin transaction by its primary key (sr_id and btc_tx_id).
-func (db DB) GetBitcoinTx(signRequestID uint64, btcTxID []byte) (*BitcoinTx, error) {
+func (db DB) GetBitcoinTx(ctx context.Context, signRequestID uint64, btcTxID []byte) (*internal.BitcoinTx, error) {
 	db.mutex.RLock()
 	defer db.mutex.RUnlock()
 
-	const getBitcoinTxByTxIDAndBtcTxIDSQL = `
-        SELECT sr_id, status, btc_tx_id, timestamp, note
-        FROM bitcoin_txs
-        WHERE sr_id = ? AND btc_tx_id = ?`
-	row := db.conn.QueryRow(getBitcoinTxByTxIDAndBtcTxIDSQL, signRequestID, btcTxID)
-	var bitcoinTx BitcoinTx
-	err := row.Scan(&bitcoinTx.SrID, &bitcoinTx.Status, &bitcoinTx.BtcTxID, &bitcoinTx.Timestamp, &bitcoinTx.Note)
+	bitcoinTx, err := db.Querier.GetBitcoinTx(ctx, &internal.GetBitcoinTxParams{SrID: int64(signRequestID), BtcTxID: btcTxID})
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return nil, nil
 		}
 		return nil, fmt.Errorf("dal: getting bitcoin_tx: %w", err)
 	}
-	return &bitcoinTx, nil
+	return bitcoinTx, nil
 }
 
 // GetIkaSignRequestWithStatus retrieves an IkaSignRequest with its associated IkaTx status.
-func (db DB) GetIkaSignRequestWithStatus(id uint64) (*IkaSignRequest, IkaTxStatus, error) {
+func (db DB) GetIkaSignRequestWithStatus(ctx context.Context, id uint64) (*internal.GetIkaSignRequestWithStatusRow, error) {
 	db.mutex.RLock()
 	defer db.mutex.RUnlock()
 
-	const getIkaSignRequestWithStatusSQL = `
-        SELECT sr.id, sr.payload, sr.dwallet_id, sr.user_sig, sr.final_sig, sr.timestamp, it.status
-        FROM ika_sign_requests sr
-        INNER JOIN ika_txs it ON sr.id = it.sr_id
-        WHERE sr.id = ?
-        ORDER BY it.time DESC -- Get the latest status
-        LIMIT 1`
-	row := db.conn.QueryRow(getIkaSignRequestWithStatusSQL, id)
-	var request IkaSignRequest
-	var status IkaTxStatus
-	err := row.Scan(
-		&request.ID,
-		&request.Payload,
-		&request.DWalletID,
-		&request.UserSig,
-		&request.FinalSig,
-		&request.Timestamp,
-		&status,
-	)
+	result, err := db.Querier.GetIkaSignRequestWithStatus(ctx, int64(id))
 	if err != nil {
 		if err == sql.ErrNoRows {
-			return nil, 0, nil
+			return nil, nil
 		}
-		return nil, 0, fmt.Errorf("dal: getting ika_sign_request with status: %w", err)
+		return nil, fmt.Errorf("dal: getting ika_sign_request with status: %w", err)
 	}
-	return &request, status, nil
+	return result, nil // Return the result and converted status
 }
 
 // GetBitcoinTxsToBroadcast retrieves IkaSignRequests that have been signed by IKA
@@ -304,118 +181,48 @@ func (db DB) GetIkaSignRequestWithStatus(id uint64) (*IkaSignRequest, IkaTxStatu
 //
 // The reason for checking these conditions is that we cannot have a Bitcoin transaction hash (btc_tx_id)
 // before the first broadcast attempt.
-func (db DB) GetBitcoinTxsToBroadcast() ([]IkaSignRequest, error) {
+func (db DB) GetBitcoinTxsToBroadcast(ctx context.Context) ([]*internal.IkaSignRequest, error) {
 	db.mutex.RLock()
 	defer db.mutex.RUnlock()
 
-	const getSignedIkaSignRequestsSQL = `
-        SELECT sr.id, sr.payload, sr.dwallet_id, sr.user_sig, sr.final_sig, sr.timestamp
-        FROM ika_sign_requests sr
-        LEFT JOIN bitcoin_txs bt ON sr.id = bt.sr_id
-        WHERE sr.final_sig IS NOT NULL
-        GROUP BY sr.id
-        HAVING COUNT(CASE WHEN bt.status = ? THEN 1 ELSE NULL END) = COUNT(bt.sr_id)`
-	rows, err := db.conn.Query(getSignedIkaSignRequestsSQL, Pending)
+	requests, err := db.Querier.GetBitcoinTxsToBroadcast(ctx, int64(Pending))
 	if err != nil {
 		return nil, fmt.Errorf("dal: querying bitcoin_txs to broadcast: %w", err)
-	}
-	defer rows.Close()
-	var requests []IkaSignRequest
-	for rows.Next() {
-		var request IkaSignRequest
-		err := rows.Scan(
-			&request.ID,
-			&request.Payload,
-			&request.DWalletID,
-			&request.UserSig,
-			&request.FinalSig,
-			&request.Timestamp,
-		)
-		if err != nil {
-			return nil, fmt.Errorf("dal: scanning row for bitcoin_txs to broadcast: %w", err)
-		}
-		requests = append(requests, request)
 	}
 	return requests, nil
 }
 
 // GetBroadcastedBitcoinTxsInfo queries Bitcoin transactions that has been braodcasted but not confirmed.
 // that do not have a "Confirmed" status.
-func (db DB) GetBroadcastedBitcoinTxsInfo() ([]BitcoinTxInfo, error) {
+func (db DB) GetBroadcastedBitcoinTxsInfo(ctx context.Context) ([]*internal.BitcoinTxInfo, error) {
 	db.mutex.RLock()
 	defer db.mutex.RUnlock()
 
-	const getBroadcastedBitcoinTxsInfoSQL = `
-        SELECT bt.sr_id, bt.btc_tx_id, bt.status
-        FROM bitcoin_txs bt
-        WHERE bt.status = ?
-        AND NOT EXISTS (
-            SELECT 1
-            FROM bitcoin_txs bt2
-            WHERE bt2.sr_id = bt.sr_id AND bt2.status = ?
-        )`
-	rows, err := db.conn.Query(getBroadcastedBitcoinTxsInfoSQL, Broadcasted, Confirmed)
+	txs, err := db.Querier.GetBroadcastedBitcoinTxsInfo(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("dal: querying broadcasted bitcoin_txs info: %w", err)
-	}
-	defer rows.Close()
-
-	var txs []BitcoinTxInfo
-	for rows.Next() {
-		var tx BitcoinTxInfo
-		err := rows.Scan(&tx.TxID, &tx.BtcTxID, &tx.Status)
-		if err != nil {
-			return nil, fmt.Errorf("dal: scanning row for broadcasted bitcoin_txs info: %w", err)
-		}
-		txs = append(txs, tx)
 	}
 	return txs, nil
 }
 
 // GetPendingIkaSignRequests retrieves IkaSignRequests that need to be signed.
-func (db DB) GetPendingIkaSignRequests() ([]IkaSignRequest, error) {
+func (db DB) GetPendingIkaSignRequests(ctx context.Context) ([]*internal.IkaSignRequest, error) {
 	db.mutex.RLock()
 	defer db.mutex.RUnlock()
 
-	const getPendingIkaSignRequestsSQL = `
-        SELECT id, payload, dwallet_id, user_sig, final_sig, timestamp
-        FROM ika_sign_requests
-        WHERE final_sig IS NULL`
-	rows, err := db.conn.Query(getPendingIkaSignRequestsSQL)
+	requests, err := db.Querier.GetPendingIkaSignRequests(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("dal: querying pending ika_sign_requests: %w", err)
-	}
-	defer rows.Close()
-	var requests []IkaSignRequest
-	for rows.Next() {
-		var request IkaSignRequest
-		err := rows.Scan(
-			&request.ID,
-			&request.Payload,
-			&request.DWalletID,
-			&request.UserSig,
-			&request.FinalSig,
-			&request.Timestamp,
-		)
-		if err != nil {
-			return nil, fmt.Errorf("dal: scanning row for pending ika_sign_requests: %w", err)
-		}
-		requests = append(requests, request)
 	}
 	return requests, nil
 }
 
 // UpdateIkaSignRequestFinalSig updates the final signature of an IkaSignRequest in the database.
-func (db DB) UpdateIkaSignRequestFinalSig(id uint64, finalSig Signature) error {
+func (db DB) UpdateIkaSignRequestFinalSig(ctx context.Context, id uint64, finalSig Signature) error {
 	db.mutex.Lock()
 	defer db.mutex.Unlock()
 
-	const updateIkaSignRequestFinalSigSQL = `
-        UPDATE ika_sign_requests 
-        SET final_sig = ?
-        WHERE id = ?`
-	_, err := db.conn.Exec(updateIkaSignRequestFinalSigSQL, finalSig, id)
-
+	err := db.Querier.UpdateIkaSignRequestFinalSig(ctx, &internal.UpdateIkaSignRequestFinalSigParams{ID: int64(id), FinalSig: finalSig})
 	if err != nil {
 		return fmt.Errorf("dal: updating ika_sign_request final sig: %w", err)
 	}
@@ -423,17 +230,12 @@ func (db DB) UpdateIkaSignRequestFinalSig(id uint64, finalSig Signature) error {
 }
 
 // UpdateBitcoinTxToConfirmed updates the bitcoin transaction to `Confirmed`.
-func (db DB) UpdateBitcoinTxToConfirmed(id uint64, txID []byte) error {
+func (db DB) UpdateBitcoinTxToConfirmed(ctx context.Context, id uint64, txID []byte) error {
 	db.mutex.Lock()
 	defer db.mutex.Unlock()
-
-	const updateBitcoinTxToConfirmedSQL = `
-        UPDATE bitcoin_txs 
-        SET status = ?, timestamp = ?
-        WHERE sr_id = ? AND btc_tx_id = ?`
 	timestamp := time.Now().Unix()
-	_, err := db.conn.Exec(updateBitcoinTxToConfirmedSQL, Confirmed, timestamp, id, txID)
 
+	err := db.Querier.UpdateBitcoinTxToConfirmed(ctx, &internal.UpdateBitcoinTxToConfirmedParams{SrID: int64(id), BtcTxID: txID, Status: int64(Confirmed), Timestamp: timestamp})
 	if err != nil {
 		return fmt.Errorf("dal: updating bitcoin_tx to confirmed: %w", err)
 	}
@@ -445,6 +247,5 @@ func (db DB) Close() error {
 	// make sure other read / writes are done
 	db.mutex.Lock()
 	defer db.mutex.Unlock()
-
 	return db.conn.Close()
 }
