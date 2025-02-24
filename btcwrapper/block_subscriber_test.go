e49ae0231684a2cfbac3baa3ab9e2ea85e0e8f1c
udit-gulati <uditgulati6@gmail.com>
2025-02-24 18:59:42 +0530

    add test for setupBitcoindConnection
    Signed-off-by: udit-gulati <uditgulati6@gmail.com>

---
+++ btcwrapper/block_subscriber_test.go
@@ -0,0 +1,50 @@
+package btcwrapper
+
+import (
+	"testing"
+	"time"
+
+	"github.com/btcsuite/btcd/chaincfg"
+	"go.uber.org/zap"
+	"gotest.tools/assert"
+
+	relayerconfig "github.com/gonative-cc/relayer/bitcoinspv/config"
+)
+
+func TestSetupBitcoindConnection(t *testing.T) {
+	tests := []struct {
+		name        string
+		client      *Client
+		expectError error
+	}{
+		{
+			name: "valid client",
+			client: &Client{
+				Client:                nil,
+				zeromqClient:          nil,
+				chainParams:           &chaincfg.MainNetParams,
+				config:                &relayerconfig.DefaultConfig().BTC,
+				logger:                &zap.SugaredLogger{},
+				blockEventsChannel:    nil,
+				retrySleepDuration:    time.Second,
+				maxRetrySleepDuration: time.Minute,
+			},
+			expectError: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := setupBitcoindConnection(tt.client)
+
+			if tt.expectError != nil {
+				assert.Error(t, err, tt.expectError.Error())
+				return
+			}
+
+			assert.NilError(t, err)
+			assert.Assert(t, tt.client.Client != nil)
+			assert.Assert(t, tt.client.zeromqClient != nil)
+		})
+	}
+}
