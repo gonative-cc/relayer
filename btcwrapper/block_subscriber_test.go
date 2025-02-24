481de4a2bd1d26d77383cb652b9c11983bf2cd88
udit-gulati <uditgulati6@gmail.com>
2025-02-24 19:46:35 +0530

    add unit test for setupBtcdConnection
    Signed-off-by: udit-gulati <uditgulati6@gmail.com>

--- btcwrapper/block_subscriber_test.go
+++ btcwrapper/block_subscriber_test.go
@@ -1,7 +1,6 @@
 package btcwrapper
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
@@ -32,20 +31,6 @@ func TestSetupBitcoindConnection(t *testing.T) {
 			},
 			expectError: nil,
 		},
-		{
-			name: "invalid config",
-			client: &Client{
-				Client:                nil,
-				zeromqClient:          nil,
-				chainParams:           &chaincfg.MainNetParams,
-				config:                nil,
-				logger:                zaptest.NewLogger(t).Sugar(),
-				blockEventsChannel:    nil,
-				retrySleepDuration:    time.Second,
-				maxRetrySleepDuration: time.Minute,
-			},
-			expectError: fmt.Errorf("invalid BTC network 'invalid'"),
-		},
 	}
 
 	for _, tt := range tests {
@@ -60,6 +45,44 @@ func TestSetupBitcoindConnection(t *testing.T) {
 			assert.NilError(t, err)
 			assert.Assert(t, tt.client.Client != nil)
 			assert.Assert(t, tt.client.zeromqClient != nil)
+		})
+	}
+}
+
+func TestSetupBtcdConnection(t *testing.T) {
+	tests := []struct {
+		name        string
+		client      *Client
+		expectError error
+	}{
+		{
+			name: "valid client",
+			client: &Client{
+				Client:                nil,
+				zeromqClient:          nil,
+				chainParams:           &chaincfg.MainNetParams,
+				config:                &relayerconfig.DefaultConfig().BTC,
+				logger:                zaptest.NewLogger(t).Sugar(),
+				blockEventsChannel:    nil,
+				retrySleepDuration:    time.Second,
+				maxRetrySleepDuration: time.Minute,
+			},
+			expectError: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := setupBtcdConnection(tt.client)
+
+			if tt.expectError != nil {
+				assert.Error(t, err, tt.expectError.Error())
+				return
+			}
+
+			assert.NilError(t, err)
+			assert.Assert(t, tt.client.Client != nil)
+			assert.Assert(t, tt.client.zeromqClient == nil)
 		})
 	}
 }
