01227d4cc89ea4ab1147301aae43e926f006ed69
udit-gulati <uditgulati6@gmail.com>
2025-03-07 21:08:42 +0530

    add case of 2 concurrent spv relayer to failover e2e script
    Signed-off-by: udit-gulati <uditgulati6@gmail.com>

--- e2e/bitcoin-spv-failover.sh
+++ e2e/bitcoin-spv-failover.sh
@@ -49,7 +49,7 @@ RELAYER_PID_NEW=$! # gets pid of last background process
 echo "Started bitcoin-spv relayer again with PID $RELAYER_PID_NEW"
 
 echo "Waiting for bitcoin-spv relayer to bootstrap..."
-sleep 10
+sleep 50 # NOTE: long wait cause waiting for relayer to submit all 100 block headers
 
 # query the lightclient again for the latest block
 chaintip_response_after_restart=$(curl --user user:password --data-binary '{"jsonrpc":"1.0","id":"curltest","method":"get_header_chain_tip","params":[]}' -H 'content-type: text/plain;' http://127.0.0.1:9797)
@@ -59,12 +59,57 @@ chaintip_hash_after_restart=$(echo "$chaintip_response_after_restart" | jq -r '.
 chaintip_height_after_restart=$(echo "$chaintip_response_after_restart" | jq -r '.result.Height')
 
 # assert that the lightclient block has increased by exactly 100
-if [[ $((chaintip_height_after-chaintip_height_before)) != 100 ]]; then
+if [[ $((chaintip_height_after_restart-chaintip_height_before)) != 100 ]]; then
   echo "ERROR: light client didn't update correctly: the latest confirmed block didn't change"
   echo "Before Hash: $chaintip_hash_before"
   echo "Before Height: $chaintip_height_before"
-  echo "After Hash: $chaintip_hash_after"
-  echo "After Height: $chaintip_height_after"
+  echo "After Hash: $chaintip_hash_after_restart"
+  echo "After Height: $chaintip_height_after_restart"
+fi
+
+# stop the relayer
+echo "Stopping bitcoin-spv relayer..."
+kill $RELAYER_PID_NEW
+
+# testing 2 concurrent relayers scenerio
+
+# produce 100 blocks
+docker exec -it bitcoind-node bitcoin-cli -generate 100
+
+# start first instance of relayer
+echo "Starting bitcoin-spv relayer (first instance)..."
+go build ./cmd/bitcoin-spv/
+./bitcoin-spv start --config ./sample-bitcoin-relayer.yml 2>stderr.log &
+RELAYER_PID_FIRST=$! # gets pid of last background process
+echo "Started bitcoin-spv relayer (first instance) with PID $RELAYER_PID_FIRST"
+
+echo "Waiting for first bitcoin-spv relayer (first instance) to bootstrap..."
+sleep 5 # NOTE: short wait cause the other one should start before this one finishes submitting block headers
+
+# start second instance of relayer
+echo "Starting bitcoin-spv relayer (second instance)..."
+go build ./cmd/bitcoin-spv/
+./bitcoin-spv start --config ./sample-bitcoin-relayer.yml 2>stderr.log &
+RELAYER_PID_SECOND=$! # gets pid of last background process
+echo "Started bitcoin-spv relayer (second instance) with PID $RELAYER_PID_SECOND"
+
+echo "Waiting for second bitcoin-spv relayer (second instance) to bootstrap..."
+sleep 50 # NOTE: long wait cause waiting for relayer to submit all 100 block headers
+
+# query the lightclient again for the latest block
+chaintip_response_after_concurrent=$(curl --user user:password --data-binary '{"jsonrpc":"1.0","id":"curltest","method":"get_header_chain_tip","params":[]}' -H 'content-type: text/plain;' http://127.0.0.1:9797)
+
+# parse the JSON to extract values using jq
+chaintip_hash_after_concurrent=$(echo "$chaintip_response_after_concurrent" | jq -r '.result.Hash')
+chaintip_height_after_concurrent=$(echo "$chaintip_response_after_concurrent" | jq -r '.result.Height')
+
+# assert that the lightclient block has increased by exactly 200
+if [[ $((chaintip_height_after_concurrent-chaintip_height_before)) != 200 ]]; then
+  echo "ERROR: light client didn't update correctly: the latest confirmed block didn't change"
+  echo "Before Hash: $chaintip_hash_before"
+  echo "Before Height: $chaintip_height_before"
+  echo "After Hash: $chaintip_hash_after_concurrent"
+  echo "After Height: $chaintip_height_after_concurrent"
 fi
 
 # read the log file of relayer
@@ -76,4 +121,7 @@ fi
 
 rm -f stderr.log
 
-kill $RELAYER_PID_NEW
+# stop the relayers
+echo "Stopping bitcoin-spv relayers..."
+kill $RELAYER_PID_FIRST
+kill $RELAYER_PID_SECOND
