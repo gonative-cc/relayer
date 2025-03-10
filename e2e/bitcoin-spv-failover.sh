64a424487e752a4a54d53fc9a33543e0e6bc4fe5
udit-gulati <uditgulati6@gmail.com>
2025-03-10 15:25:41 +0530

    in failover e2e script, send 20 blocks manually to sui LC before relayeer restart
    Signed-off-by: udit-gulati <uditgulati6@gmail.com>

--- e2e/bitcoin-spv-failover.sh
+++ e2e/bitcoin-spv-failover.sh
@@ -22,7 +22,7 @@ if ! docker exec -it bitcoind-node bitcoin-cli -regtest getblockchaininfo; then
 fi
 
 # query the lightclient for the latest block
-chaintip_response_before=$(sui client call --function latest_block_hash --module light_client --package $PACKAGE_ID --gas-budget 10000000 --args $OBJECT_ID --dev-inspect --json)
+chaintip_response_before=$(sui client call --function latest_block --module light_client --package $PACKAGE_ID --gas-budget 10000000 --args $OBJECT_ID --dev-inspect --json)
 
 chaintip_height_before=$(echo "$chaintip_response_before" | jq -r '.events[].parsedJson.height')
 
@@ -34,7 +34,7 @@ kill $RELAYER_PID_OLD
 docker exec -it bitcoind-node bitcoin-cli -generate 100
 
 # query the lightclient again for the latest block
-chaintip_response_after_stop=$(sui client call --function latest_block_hash --module light_client --package $PACKAGE_ID --gas-budget 10000000 --args $OBJECT_ID --dev-inspect --json)
+chaintip_response_after_stop=$(sui client call --function latest_block --module light_client --package $PACKAGE_ID --gas-budget 10000000 --args $OBJECT_ID --dev-inspect --json)
 
 # parse the JSON to extract values using jq
 chaintip_height_after_stop=$(echo "$chaintip_response_after_stop" | jq -r '.events[].parsedJson.height')
@@ -50,7 +50,7 @@ echo "Waiting for bitcoin-spv relayer to bootstrap..."
 sleep 50 # NOTE: long wait cause waiting for relayer to submit all 100 block headers
 
 # query the lightclient again for the latest block
-chaintip_response_after_restart=$(sui client call --function latest_block_hash --module light_client --package $PACKAGE_ID --gas-budget 10000000 --args $OBJECT_ID --dev-inspect --json)
+chaintip_response_after_restart=$(sui client call --function latest_block --module light_client --package $PACKAGE_ID --gas-budget 10000000 --args $OBJECT_ID --dev-inspect --json)
 
 # parse the JSON to extract values using jq
 chaintip_height_after_restart=$(echo "$chaintip_response_after_restart" | jq -r '.events[].parsedJson.height')
@@ -66,33 +66,38 @@ fi
 echo "Stopping bitcoin-spv relayer..."
 kill $RELAYER_PID_NEW
 
-# testing 2 concurrent relayers scenerio
+# testing restart relayer after few more blocks have been posted
 
-# produce 100 blocks
-docker exec -it bitcoind-node bitcoin-cli -generate 100
+# manually send the initial 20 block headers using sui client
+for i in {1..20}; do
+  # generate a block
+  docker exec -it bitcoind-node bitcoin-cli -generate 1
 
-# start first instance of relayer
-echo "Starting bitcoin-spv relayer (first instance)..."
+  # query latest blockhash
+  latest_blockhash=$(docker exec -it bitcoind-node bitcoin-cli -regtest getbestblockhash)
+
+  # query the blockheader of that blockhash
+  latest_blockheader=$(docker exec -it bitcoind-node bitcoin-cli -regtest getblockheader $latest_blockhash false)
+
+  # send the blockheader to sui LC using sui client
+  sui client call --function insert_headers --module light_client --package $PACKAGE_ID --gas-budget 100000000 --args $OBJECT_ID ['0x$latest_blockheader']
+done
+
+# produce 80 blocks
+docker exec -it bitcoind-node bitcoin-cli -generate 80
+
+# start relayer
+echo "Starting bitcoin-spv relayer..."
 go build ./cmd/bitcoin-spv/
 ./bitcoin-spv start --config ./sample-bitcoin-relayer.yml 2>stderr.log &
-RELAYER_PID_FIRST=$! # gets pid of last background process
-echo "Started bitcoin-spv relayer (first instance) with PID $RELAYER_PID_FIRST"
+RELAYER_PID_AGAIN=$! # gets pid of last background process
+echo "Started bitcoin-spv relayer with PID $RELAYER_PID_AGAIN"
 
-echo "Waiting for first bitcoin-spv relayer (first instance) to bootstrap..."
-sleep 5 # NOTE: short wait cause the other one should start before this one finishes submitting block headers
-
-# start second instance of relayer
-echo "Starting bitcoin-spv relayer (second instance)..."
-go build ./cmd/bitcoin-spv/
-./bitcoin-spv start --config ./sample-bitcoin-relayer.yml 2>stderr.log &
-RELAYER_PID_SECOND=$! # gets pid of last background process
-echo "Started bitcoin-spv relayer (second instance) with PID $RELAYER_PID_SECOND"
-
-echo "Waiting for second bitcoin-spv relayer (second instance) to bootstrap..."
-sleep 50 # NOTE: long wait cause waiting for relayer to submit all 100 block headers
+echo "Waiting for second bitcoin-spv relayer to bootstrap..."
+sleep 50 # NOTE: long wait cause waiting for relayer to submit all 80 block headers
 
 # query the lightclient again for the latest block
-chaintip_response_after_concurrent=$(sui client call --function latest_block_hash --module light_client --package $PACKAGE_ID --gas-budget 10000000 --args $OBJECT_ID --dev-inspect --json)
+chaintip_response_after_concurrent=$(sui client call --function latest_block --module light_client --package $PACKAGE_ID --gas-budget 10000000 --args $OBJECT_ID --dev-inspect --json)
 
 # parse the JSON to extract values using jq
 chaintip_height_after_concurrent=$(echo "$chaintip_response_after_concurrent" | jq -r '.events[].parsedJson.height')
@@ -113,7 +118,6 @@ fi
 
 rm -f stderr.log
 
-# stop the relayers
-echo "Stopping bitcoin-spv relayers..."
-kill $RELAYER_PID_FIRST
-kill $RELAYER_PID_SECOND
+# stop the relayer
+echo "Stopping bitcoin-spv relayer..."
+kill $RELAYER_PID_AGAIN
