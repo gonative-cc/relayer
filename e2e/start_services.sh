0cd2d59960d4dbf13408f62a0211d53b62ce3814
sczembor <stanislaw.czembor@gmail.com>
2025-03-12 13:54:20 +0100

    remove unsupported commands from docker compose
    Signed-off-by: sczembor <stanislaw.czembor@gmail.com>

--- e2e/start_services.sh
+++ e2e/start_services.sh
@@ -4,6 +4,10 @@ set -e
 make bitcoind-init
 cd contrib/
 echo "Starting Docker Compose services..."
-docker compose up -d
+docker compose up -d --wait
 echo "Services started."
 cd ../
+
+./e2e/load_bitcoin_wallet.sh
+
+echo "Services and Bitcoin wallet ready."
