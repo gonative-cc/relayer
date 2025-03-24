e204cfe683dabec237e7be76b6df5defa3931dfb
Robert Zaremba <robert@zaremba.ch>
2025-03-24 10:16:54 +0400

    lint
    Signed-off-by: Robert Zaremba <robert@zaremba.ch>

--- tools/tools.go
+++ tools/tools.go
@@ -1,6 +1,5 @@
 //go:build tools
 
-
 // This file uses the recommended method for tracking developer tools in a Go
 // module.
 // TODO: should be removed when updating toolchain to go v1.24 in May
