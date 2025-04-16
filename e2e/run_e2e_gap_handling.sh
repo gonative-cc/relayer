e863a3ed6304fe7201f6b3cb448f37e37ff7fbd3
Vu Vo <vu.voth@gmail.com>
2025-04-16 17:28:33 +0700

    better script
    Signed-off-by: Vu Vo <vu.voth@gmail.com>

--- e2e/run_e2e_gap_handling.sh
+++ e2e/run_e2e_gap_handling.sh
@@ -1,71 +1,34 @@
 #!/bin/bash
 set -e
-
-function get_latest_block_height_lc() {
-    latest_block_hash=$(docker exec "$CONTAINER_ID" /bin/bash -c "sui client call --function latest_block_hash --module light_client --package '$PACKAGE_ID' --gas-budget 100000000 --args $LIGHT_CLIENT_ID  --dev-inspect --json")
-    latest_block_height=$(echo "$latest_block_hash" | jq -r '.events[].parsedJson.height')
-    echo $latest_block_height
-}
-
-function get_btc_height() {
-    echo $(docker exec -it bitcoind-node bitcoin-cli getblockchaininfo | jq ".headers")
-}
-
-echo "Running E2E tests..."
 CONTAINER_ID="sui-node"
 
+echo "Running E2E tests..."
 
 echo "Start relayer to sync first time"
-# Start relayer
-go build ./cmd/bitcoin-spv/
-./bitcoin-spv start --config ./e2e-bitcoin-spv.yml 2>stderr.log &
-sleep 30
-relayer_pid=$!
-kill $relayer_pid
+
+./e2e/sync_and_validate_status.sh
+
 echo "Finished first sync and stop relayer"
 
-
 echo "==============================="
-lc_height=$(get_latest_block_height_lc)
-btc_height=$(get_btc_height)
-
-if [[ $((lc_height - btc_height)) != 0 ]]; then
-    echo "Relayer not sync the btc node and lc"
-    exit 1
-fi
 
 echo "Other party update light client"
 echo "Insert one header to lc"
 docker exec -it bitcoind-node bitcoin-cli -generate 1 > /dev/null 2>&1
-
+sleep 2
 new_block_header=$(docker exec bitcoind-node  /bin/bash -c "bitcoin-cli getblockheader $(docker exec bitcoind-node  /bin/bash -c "bitcoin-cli getbestblockhash") false")
 docker exec "$CONTAINER_ID" /bin/bash -c "sui client call --function insert_headers --module light_client --package '$PACKAGE_ID' --gas-budget 100000000 --args $LIGHT_CLIENT_ID '[0x${new_block_header}]' --json" > /dev/null 2>&1
 sleep 10
 echo "Inserted header"
 
-
-echo "Start relayer to sync second time"
-go build ./cmd/bitcoin-spv/
-./bitcoin-spv start --config ./e2e-bitcoin-spv.yml 2>stderr.log &
-
+echo "==============================="
 echo "Create more 10 block"
 docker exec -it bitcoind-node bitcoin-cli -generate 10 > /dev/null 2>&1
-sleep 30
-relayer_pid=$!
-kill $relayer_pid
-
+sleep 2
+echo "=============================="
+echo "Start relayer to sync second time"
+./e2e/sync_and_validate_status.sh
 echo "Finished second sync and stop relayer"
-
-
-echo "Check height!"
-lc_height=$(get_latest_block_height_lc)
-btc_height=$(get_btc_height)
-
-if [[ $((lc_height - btc_height)) != 0 ]]; then
-    echo "Relayer not sync the btc node and lc"
-    exit 1
-fi
-
 echo "Everything work well"
 
 echo "E2E Gap handler tests completed."
---
+++ e2e/sync_and_validate_status.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+CONTAINER_ID="sui-node"
+
+function get_latest_block_height_lc() {
+    latest_block_hash=$(docker exec "$CONTAINER_ID" /bin/bash -c "sui client call --function latest_block_hash --module light_client --package '$PACKAGE_ID' --gas-budget 100000000 --args $LIGHT_CLIENT_ID  --dev-inspect --json")
+    latest_block_height=$(echo "$latest_block_hash" | jq -r '.events[].parsedJson.height')
+    echo $latest_block_height
+}
+
+function get_btc_height() {
+    echo $(docker exec -it bitcoind-node bitcoin-cli getblockchaininfo | jq ".headers")
+}
+
+# Start relayer
+go build ./cmd/bitcoin-spv/
+./bitcoin-spv start --config ./e2e-bitcoin-spv.yml > /dev/null 2>&1
+sleep 30
+relayer_pid=$!
+kill $relayer_pid
+
+lc_height=$(get_latest_block_height_lc)
+btc_height=$(get_btc_height)
+
+if [[ $((lc_height - btc_height)) != 0 ]]; then
+    echo "Relayer not sync the btc node and lc"
+    exit 1
+fi
