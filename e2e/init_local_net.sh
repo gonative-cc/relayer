c97924d3407a6ee176eb074b65bdba5d7f1b8e52
vuvoth <vu.voth@gmail.com>
2025-04-18 00:50:45 +0700

    Update e2e/init_local_net.sh
    Co-authored-by: sczembor <43810037+sczembor@users.noreply.github.com>
    Signed-off-by: vuvoth <vu.voth@gmail.com>

--- e2e/init_local_net.sh
+++ e2e/init_local_net.sh
@@ -13,8 +13,6 @@ echo "Active Sui Env: $ACTIVE_ENV"
 echo "Requesting SUI from faucet..."
 docker exec "$CONTAINER" sui client faucet
 docker exec "$CONTAINER" sui client faucet --address 0x9a5779d1f633d365652efcbe3a90abf0789e6890b880af66710db5e3e3e907e1
-
-
 sleep 5  # Wait for faucet
 
 
