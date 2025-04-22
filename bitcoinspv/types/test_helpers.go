da10f798c467a2e7e2eac8422fba67a2cee9d4d8
sczembor <stanislaw.czembor@gmail.com>
2025-04-22 19:46:38 +0200

    disable linter for overflow in helper
    Signed-off-by: sczembor <stanislaw.czembor@gmail.com>

--- bitcoinspv/types/test_helpers.go
+++ bitcoinspv/types/test_helpers.go
@@ -12,6 +12,7 @@ func CreateTestIndexedBlocks(t *testing.T, count int64, startHeight int64) []*In
 	t.Helper()
 	blocks := make([]*IndexedBlock, count)
 	for i := int64(0); i < count; i++ {
+		//nolint: gosec // This is a test function, and overflow is unlikely.
 		hdr := wire.BlockHeader{Version: int32(i + 1), Timestamp: time.Now().Add(time.Duration(i) * time.Minute)}
 		blocks[i] = &IndexedBlock{
 			BlockHeight: startHeight + i,
