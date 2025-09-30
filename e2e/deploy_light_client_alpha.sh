ec10468c3586317939a8acd654a8e08de48d1335
sczembor <stanislaw.czembor@gmail.com>
2025-09-30 17:02:27 +0200

    use one package id
    Signed-off-by: sczembor <stanislaw.czembor@gmail.com>

--- e2e/deploy_light_client_alpha.sh
+++ e2e/deploy_light_client_alpha.sh
@@ -6,6 +6,12 @@ CONTAINER_ID="sui-node"
 PACKAGE_PATH="sui-bitcoin-spv/packages/bitcoin_spv"
 CONFIG_FILE="e2e-bitcoin-spv.yml"
 
+INIT_HEADERS='0x0100000000000000000000000000000000000000000000000000000000000000000000003ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4adae5494dffff7f2002000000'
+BTC_NETWORK=2  # regtest (0: mainnet, 1: testnet, 2: regtest)
+START_HEIGHT=0
+
+
+
 echo "Cloning light client repo"
 docker exec "$CONTAINER_ID" /bin/bash -c \
        "rm -rf $PACKAGE_PATH && \
@@ -14,8 +20,24 @@ docker exec "$CONTAINER_ID" /bin/bash -c \
 
 echo "Deploying light client to Sui network..."
 PUBLISH_OUTPUT=$(docker exec "$CONTAINER_ID" /bin/bash -c "cd '$PACKAGE_PATH' && sui client publish --skip-dependency-verification --gas-budget 1000000000 --json  --with-unpublished-dependencies")
-PUBLISH_JSON=$(echo "$PUBLISH_OUTPUT" | tail -n 1)
-PACKAGE_ID=$(echo "$PUBLISH_JSON" | jq -r '.objectChanges[] | select(.type == "published") | .packageId')
+
+PACKAGE_ID=$(echo "$PUBLISH_OUTPUT" | jq -r '.objectChanges[] | select(.type == "published") | .packageId')
+
+LOCAL_REPO_PATH="./e2e/sui-bitcoin-spv"
+rm -rf "$LOCAL_REPO_PATH"
+git clone "$REPO_URL" "$LOCAL_REPO_PATH"
+
+if [ -z "$PACKAGE_ID" ]; then
+  echo "Failed to extract Package ID!"
+  echo "Publish Output:"
+  echo "$PUBLISH_OUTPUT"
+  exit 1
+fi
+
+echo "Package ID: $PACKAGE_ID"
+
+echo $(pwd)
+
 SPV_PACKAGE_ID=$PACKAGE_ID
 PARSER_PACKAGE_ID=$PACKAGE_ID
 
@@ -30,14 +52,12 @@ if [ -z "$SPV_PACKAGE_ID" ] || [ -z "$PARSER_PACKAGE_ID" ]; then
   exit 1
 fi
 
-echo "Bitcoin SPV Package ID: $SPV_PACKAGE_ID"
-echo "Bitcoin Parser Package ID: $PARSER_PACKAGE_ID"
+echo "Bitcoin SPV Package ID: $PACKAGE_ID"
+echo "Bitcoin Parser Package ID: $PACKAGE_ID"
 
-echo $(pwd)
 
-# Update the .env file with both new variables
-sed -i "s#^SPV_PACKAGE_ID=.*#SPV_PACKAGE_ID=${SPV_PACKAGE_ID}#" ./e2e/.e2e.env
-sed -i "s#^PARSER_PACKAGE_ID=.*#PARSER_PACKAGE_ID=${PARSER_PACKAGE_ID}#" ./e2e/.e2e.env
+sed -i "s#^SPV_PACKAGE_ID=.*#SPV_PACKAGE_ID=${PACKAGE_ID}#" ./e2e/.e2e.env
+sed -i "s#^PARSER_PACKAGE_ID=.*#PARSER_PACKAGE_ID=${PACKAGE_ID}#" ./e2e/.e2e.env
 
 
 cd "$LOCAL_REPO_PATH"
@@ -61,7 +81,7 @@ echo "Light Client ID: $LIGHT_CLIENT_ID"
 echo "Updating $CONFIG_FILE with Package ID and Light Client ID..."
 
 sed -i "s|lc_object_id:.*|lc_object_id: \"$LIGHT_CLIENT_ID\"|" "$CONFIG_FILE"
-sed -i "s|lc_package_id:.*|lc_package_id: \"$SPV_PACKAGE_ID\"|" "$CONFIG_FILE"
+sed -i "s|lc_package_id:.*|lc_package_id: \"$PACKAGE_ID\"|" "$CONFIG_FILE"
 
 
 echo "Configuration file updated successfully."
